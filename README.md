959cae2a2ed395355bcf257ebe2fa2cb68a80c05
catherine <cathey191@hotmail.com>
2018-10-10 08:39:48 +1300

    File setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# PHP Website
+
+This site is created using PHP and Bootstrap.
+It uses templating to be able to use only one file for the header and footer but the main content stays the same.
---
+++ templates/nav.php
@@ -0,0 +1,6 @@
+<nav class="nav nav-masthead justify-content-center">
+  <a class="nav-link <?php if($page === 'home'): ?>active<?php endif; ?>" href="index.php">Home</a>
+  <a class="nav-link <?php if($page === 'about'): ?>active<?php endif; ?>" href="about.php">About</a>
+  <a class="nav-link <?php if($page === 'features'): ?>active<?php endif; ?>" href="features.php">Features</a>
+  <a class="nav-link <?php if($page === 'contact'): ?>active<?php endif; ?>" href="contact.php">Contact</a>
+</nav>
